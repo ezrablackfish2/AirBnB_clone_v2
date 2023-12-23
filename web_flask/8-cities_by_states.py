5c85be0c40a9040d2acd303c5e16bd686bf6e1df
ezra <ezrayeneneh1992@gmail.com>
2023-12-22 23:07:00 -0800

    part 8 done

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+from flask import Flask
+from markupsafe import escape
+from flask import render_template
+from models import storage
+from models.state import State
+from models.city import City
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+
+
+app = Flask(__name__)
+
+@app.teardown_appcontext
+def tear(exception):
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    states = storage.all("State")
+
+    return render_template("7-states_list.html", states=states)
+@app.route('/cities_by_states', strict_slashes=False)
+def city_by_state():
+    states = storage.all("State")
+    cities = storage.all("City")
+
+    return render_template("8-cities_by_states.html", states=states, cities=cities)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
