3a00d8c6f7fbd2b94fc8853d9e6cbe37ed3e3bb9
ezra <ezrayeneneh1992@gmail.com>
2023-12-22 23:54:27 -0800

    yea nigga

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,45 +1,42 @@
 #!/usr/bin/python3
+"""Starts a Flask web application.
 
-from flask import Flask
-from markupsafe import escape
-from flask import render_template
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /states: HTML page with a list of all State objects.
+    /states/<id>: HTML page displaying the given state with <id>.
+"""
 from models import storage
-from models.state import State
-from models.city import City
-from sqlalchemy.orm import scoped_session, sessionmaker
-
-
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
+
+@app.route("/states", strict_slashes=False)
+def states():
+    """Displays an HTML page with a list of all States.
+
+    States are sorted by name.
+    """
+    states = storage.all("State")
+    return render_template("9-states.html", state=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    """Displays an HTML page with info about <id>, if it exists."""
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
+
+
 @app.teardown_appcontext
-def tear(exception):
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    states = storage.all("State")
-
-    return render_template("7-states_list.html", states=states)
-@app.route('/cities_by_states', strict_slashes=False)
-def city_by_state():
-    states = storage.all("State")
-    cities = storage.all("City")
-
-    return render_template("8-cities_by_states.html", states=states, cities=cities)
-@app.route('/states', strict_slashes=False)
-def states():
-    states= storage.all("State")
-    cities = storage.all("City")
-    return render_template("9-states.html", states=states, cities=cities)
-
-@app.route('/states/<id>', strict_slashes=False)
-def states_id(id):
-    states = storage.all("State")
-    cities = storage.all("City")
-    return render_template("9-states.html", states=states, cities=cities, id=id)
-
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0")
